49a9a387e38c689dcdb5a8a8b1abd4489d8650af
Raavi Kaur <raavi211299@gmail.com>
2024-08-10 22:28:05 -0500

    optimize the code

--- typing_speed_test.py
+++ typing_speed_test.py
@@ -7,7 +7,7 @@ foreground = 'red'
 sliderwords = ''
 timeleft = 60
 completed_word = set()
-count = wpm_count = correct = wrong = 0
+count = index = wpm_count = correct = wrong = 0
 word_list = []
 
 def load_word_list():
@@ -15,6 +15,7 @@ def load_word_list():
         data = csv.reader(word_data)
         for row in data:
             word_list.append(''.join(row))
+        random.shuffle(word_list)
 
 def slider():
     global sliderwords, count
@@ -28,7 +29,7 @@ def slider():
     movingLabel.after(200, slider)  # calls itself every 200 milli sec
 
 def play_game(event):  # Trigger when press Enter
-    global wpm_count, correct, wrong
+    global wpm_count, correct, wrong, index
     wpm_count += 1
     wordCount.config(text=wpm_count)
     if timeleft == 60:
@@ -37,14 +38,11 @@ def play_game(event):  # Trigger when press Enter
         correct += 1
     else:
         wrong += 1
-    completed_word.add(wordList['text'])
-    random.shuffle(word_list)
-    while word_list[0] in completed_word:
-        random.shuffle(word_list)
-    completed_word.add(word_list[0])
-    wordList.config(text=word_list[0])
-    wordEntry.delete(0, END)
-    if len(completed_word) >= len(word_list):  # if all words used from file
+    index += 1
+    if index < len(word_list)-1:  # if all words used from file
+        wordList.config(text=word_list[index])
+        wordEntry.delete(0, END)
+    else:
         wordList.config(text="All words used!")
         wordEntry.config(state=DISABLED)
 
@@ -55,6 +53,7 @@ def timer():
         timeCounter.config(text=timeleft)
         timeCounter.after(1000, timer)
     else:
+        wordList.config(state=DISABLED)
         wordEntry.config(state=DISABLED)  # to disable entry field when times up
         instructionLabel.config(text=f"Correct Words {correct} WPM\nWrong Words {wrong}")
 
@@ -66,21 +65,20 @@ window.config(bg=background)
 movingLabel = Label(text='', bg=background, fg=foreground, font=('ariel', 25, 'bold italic'), width=35)
 movingLabel.place(x=0, y=10)
 slider()
-wordCount = Label(text=wpm_count, font=('ariel', 20, 'normal'))
+wordCount = Label(text=wpm_count, width=3, font=('ariel', 20, 'normal'))
 wordCount.config(padx=10, pady=10)
-wordCount.place(x=200, y=100)
+wordCount.place(x=190, y=100)
 wordLabel = Label(text='word', bg=background, font=('ariel', 23, 'bold'))
 wordLabel.place(x=190, y=150)
-timeCounter = Label(text=timeleft, font=('ariel', 20, 'normal'))
+timeCounter = Label(text=timeleft, width=3, font=('ariel', 20, 'normal'))
 timeCounter.config(padx=10, pady=10)
 timeCounter.place(x=450, y=100)
 timeLabel = Label(text='timer', bg=background, font=('ariel', 23, 'bold'))
-timeLabel.place(x=440, y=150)
+timeLabel.place(x=450, y=150)
 
 load_word_list()
-random.shuffle(word_list)
-wordList = Label(text=word_list[0], bg=background, font=('ariel', 23))
-wordList.place(x=300, y=220)
+wordList = Label(text=word_list[index], width=18, bg=background, font=('ariel', 23), justify=CENTER)
+wordList.place(x=200, y=220)
 wordEntry = Entry(font=('ariel', 20), justify=CENTER)
 wordEntry.focus_set()  # set the cursor
 wordEntry.place(x=200, y=260)
